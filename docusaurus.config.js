9f390b461825fa812cfa6c0b535b1efcb04eec53
Zack Hodgson Brady <zackbrady123@gmail.com>
2024-03-05 08:51:02 -0500

    upgraded docs to docusaurus v3

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,8 +1,8 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require('prism-react-renderer').themes.github;
+const darkCodeTheme = require('prism-react-renderer').themes.dracula;
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -28,12 +28,13 @@ const config = {
   },
 
   plugins: [
-    [
-      require.resolve("@cmfcmf/docusaurus-search-local"),
+    [ require.resolve('docusaurus-lunr-search'),
       {
-        // Options here
-      },
-    ],
+        languages: ['en'],
+        indexBaseUrl: true,
+        highlightResult: true
+      }
+    ]
   ],
 
   presets: [
